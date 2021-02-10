3ef75b53796b43310cc1d97f8a94041c12a3cd02
nopjmp <kthompson@hey.com>
2021-02-10 16:01:55 -0600

    update to newer Paperclip
    Note: PaperBin users have to use the cache/patched_1.12.2.jar method for now.

--- scripts/paperclip.sh
+++ scripts/paperclip.sh
@@ -7,15 +7,16 @@
 set -e
 basedir="$(cd "$1" && pwd -P)"
 workdir="$basedir/Paper/work"
+localworkdir="$basedir/work"
 mcver=$(cat "$workdir/BuildData/info.json" | grep minecraftVersion | cut -d '"' -f 4)
 paperjar="$basedir/Dionysus-Server/target/dionysus-$mcver.jar"
 vanillajar="$workdir/Minecraft/$mcver/$mcver.jar"
 
 (
-    cd "$workdir/Paperclip"
+    cd "$localworkdir/Paperclip"
     mvn clean package "-Dmcver=$mcver" "-Dpaperjar=$paperjar" "-Dvanillajar=$vanillajar"
 )
-cp "$workdir/Paperclip/assembly/target/paperclip-${mcver}.jar" "$basedir/dionysus-paperclip.jar"
+cp "$localworkdir/Paperclip/assembly/target/paperclip-${mcver}.jar" "$basedir/dionysus-paperclip.jar"
 
 echo ""
 echo ""
